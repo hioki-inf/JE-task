a8005f24c9de98d274a9085cbe670a7021eac4cc
EC2 Default User <ec2-user@ip-172-31-12-82.ap-northeast-1.compute.internal>
2024-04-26 19:08:21 +0000

    act as bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -110,5 +110,4 @@ ActiveRecord::Schema.define(version: 2024_04_17_103714) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "taggings", "tags"
 end
